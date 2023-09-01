6030b4bf9a5352248be57a4f2b5098832312f87a
lswebdevelops <sardanhaluciano9690@gmail.com>
2023-09-01 16:04:31 -0300

    adding some styling

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import './index.css'
+import Home from "./components/Home";
+import About from "./components/About";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+ export default App;
